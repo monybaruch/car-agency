87b190d01f1c7b4b4fb744677927c1b90cbd88d1
מוני ברוך <monybaruch@gmail.com>
2023-08-05 02:58:59 +0300

    adding 2 more getCheapestCar and getMostExpensiveCar methods to the CarManager object

--- data/cars.js
+++ data/cars.js
@@ -6,7 +6,7 @@ const CarManager = {
   ...agencies,
   // Retrieve all cars available for purchase.
   // @return {object[]} - Array of cars
-  getAllCars: function () {
+  getAllCars() {
     const allCars = [];
     for (const agency of agencies) {
       for (const car of agency.cars) {
@@ -20,11 +20,43 @@ const CarManager = {
   // @param {number} price - The price of the car
   // @param {string} brand - The brand of the car
   // @return {object[]} - Array of cars that meet the criteria
-  searchCars: function (year, price, brand) {
+  searchCars(year, price, brand) {
     return this.getAllCars().filter((car) => car.year === year && car.price === price && car.name === brand);
+  },
+  // Return the most expensive car available for sale.
+  // @return {object} - The most expensive car
+  getMostExpensiveCar: function () {
+    const carPricesArr = [];
+    agencies.forEach((agency) => {
+      agency.cars.forEach((carBrand) => {
+        carBrand.models.forEach((carModel) => {
+          carPricesArr.push(carModel.price);
+        });
+      });
+    });
+    const maxPrice = Math.max(...carPricesArr);
+    return maxPrice;
+  },
+  // Return the cheapest car available for sale.
+  // @return {object} - The cheapest car
+  getCheapestCar() {
+    const carPricesArr = [];
+    agencies.forEach((agency) => {
+      agency.cars.forEach((carBrand) => {
+        carBrand.models.forEach((carModel) => {
+          carPricesArr.push(carModel.price);
+        });
+      });
+    });
+    const maxPrice = Math.min(...carPricesArr);
+    return maxPrice;
   },
 };
 
 console.log(CarManager.getAllCars());
 
 console.log(CarManager.searchCars(2020, 111900, 'Corolla'));
+
+console.log(CarManager.getMostExpensiveCar());
+
+console.log(CarManager.getCheapestCar());
